f3df54085f676e86950a1c1ca5da74f829801334
Ümit Aslan <prototprotot2@gmail.com>
2022-03-17 12:45:44 +0300

    Added new scenes

--- Assets/Scripts/BlockController.cs
+++ Assets/Scripts/BlockController.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class BlockController : MonoBehaviour
 {
     public int Health; // Health of the block
+   public static int blockNumber; // Number of blocks
+   public bool breakingBlocks; // Is the block breaking?
+   private Game_Manager gm;
+
     void Start()
     {
-        
-    }
+        gm=GameObject.Find("Game_Manager").GetComponent<Game_Manager>(); 
+      breakingBlocks=(this.tag=="Block");
+      if(breakingBlocks)
+      {
+        blockNumber++;
+      }
 
-    void Update()
-    {
-        
     }
 
    private void OnCollisionEnter2D(Collision2D other) 
@@ -20,9 +25,11 @@ public class BlockController : MonoBehaviour
      if (true)
      {
          Health--;
-         if (Health <= 0)
+         if (Health <= 0)   
          {
+             blockNumber--;
              Destroy(gameObject);
+             gm.afterScene();
          }
      }
    }
--- Assets/Scripts/Game_Manager.cs
+++ Assets/Scripts/Game_Manager.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Game_Manager : MonoBehaviour
 {
- 
+ public Transform ball;
+ public Transform cam;
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        checkBallPos();
     }
-}
+
+
+    private void checkBallPos(){
+
+        if(ball.position.y <=cam.position.y-7){
+           SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+    public void afterScene(){
+        if (BlockController.blockNumber <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+       
+    }
+} 
